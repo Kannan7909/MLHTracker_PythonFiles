84f54debf3a34f1fe1b78125e616d30b0ea1bfe0
Kannan7909 <Kannan@echoapps360.com>
2023-02-27 08:43:09 +0530

    Add mail file

---
+++ mail.py
@@ -0,0 +1,47 @@
+import logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+def sendHtmlMail(emailId, subject, content):
+    sender_email = "excellentdemo35@gmail.com"
+    sender_password = "efvhoqjojppepsae"
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = str(Header('MLH Tracker', 'utf-8')) + '<excellentdemo35@gmail.com>'  # set sender name and email address"
+    msg["To"] = emailId
+
+    # Set the HTML content
+    html = content
+    msg.attach(MIMEText(html, "html"))
+
+    # Send the email
+    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(sender_email, sender_password)
+        smtp.send_message(msg)
+
+def sendNormalMail(emailId, subject, content):
+    sender_email = "excellentdemo35@gmail.com"
+    sender_password = "efvhoqjojppepsae"
+    receiver_email = emailId
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(sender_email, sender_password)
+
+    subject = subject
+    message = content
+
+    header = f"From: {sender_email}\nTo: {receiver_email}\nSubject: {subject}\n"
+    body = f"{header}\n{message}"
+
+    try :
+        server.sendmail(sender_email, receiver_email, body)
+    except Exception as e:
+        logging.error(str(e))
+    server.quit()
